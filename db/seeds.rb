0038c58ef714e1966dd39954d3f8af93553eee00
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2019-08-08 21:00:28 +0200

    improve registration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Fighter.destroy_all
+Fight.destroy_all
+
 puts "Blood bath ongoing..."
 
-pikachu = Fighter.create!(
+Fighter.create(
   name:"Pikachu",
   remote_avatar_url: "https://res.cloudinary.com/diuc2dxj5/image/upload/v1565096488/killing-spree/pikachu-sprite_uhbg68.gif",
   level: 1,
@@ -18,7 +14,7 @@ pikachu = Fighter.create!(
   winrate: 0,
 )
 
-salameche = Fighter.create!(
+Fighter.create(
   name:"Salamèche",
   remote_avatar_url: "https://res.cloudinary.com/diuc2dxj5/image/upload/v1565096494/killing-spree/salameche-sprite_n68yoc.gif",
   level: 1,

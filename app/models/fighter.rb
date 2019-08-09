f6bd61f9f7bee9d517a0d313a5d201e492e32b0e
Sonia Rottenberg <sonia.rottenberg@gmail.com>
2019-08-09 03:35:23 +0200

    added validations

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,5 +1,10 @@
 class Fighter < ApplicationRecord
   has_many :registrations
   has_many :fights, through: :registrations
+
+  validates :health, numericality: { less_than_or_equal_to: 10 }
+  validates :attack, numericality: { less_than_or_equal_to: 10 }
+  validates :defense, numericality: { less_than_or_equal_to: 10 }
+
   mount_uploader :avatar, PhotoUploader
 end
